74931485123488ac5ff1fc59d351d8d55dc6ded6
Ubuntu <ubuntu@ip-172-31-21-79.us-east-2.compute.internal>
2026-08-31 13:21:08 +0000

    fix(server): increase express body parser limits to 100mb

--- server/server.js
+++ server/server.js
@@ -42,7 +42,8 @@ app.use(async (req, res, next) => {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 // Serve Static Uploads Directory (AWS / Server File Storage)
 const path = require('path');
